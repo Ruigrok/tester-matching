2e66dc417c4b09a86eb1881baaa4004e0034bd0c
AC Ruigrok <andrew.c.ruigrok@gmail.com>
2017-11-22 12:14:41 -0600

    configuring for build

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 // Static directory
-app.use(express.static('client/public'));
-//app.use(express.static(path.join(__dirname, 'client/build')));
+//app.use(express.static('client/public'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 require('./api-routes.js')(app);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/public/index.html'));
-});
-
 /* app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/public/index.html'));
+}); */
+
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});  */
+}); 
 
 app.listen(PORT, () => {
   console.log('App listening on PORT ' + PORT);
